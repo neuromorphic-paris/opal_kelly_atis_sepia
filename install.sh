cf11be7cfcb2e8c952935c53c5c63b615bc57ee7
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2019-04-18 16:11:52 +0200

    Add a missing flag for Ubuntu

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 cd -P -- "$(dirname -- "$0")"
 rm -rf resources
-curl 134.157.180.96/opal_kelly_atis_sepia.tar.xz | tar x
+curl 134.157.180.96/opal_kelly_atis_sepia.tar.xz | tar xJ
 if [ "$OSTYPE" = 'linux-gnu' ]; then
     sudo cp resources/libudev.so.0 /usr/lib/
     sudo ldconfig
